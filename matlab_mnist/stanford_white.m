07ae8db2b59b003de8a60a4d62c86f8e95cb7721
Artem Chernodub <a.chernodub@gmail.com>
2017-07-25 14:22:47 +0300

    Modify stanford whitening

--- matlab_mnist/stanford_white.m
+++ matlab_mnist/stanford_white.m
@@ -1,5 +1,4 @@
-
-function [xZCAwhite, whitemat, avg, sigma, xTilde] = stanford_white(x,epsilon)
+function [xZCAwhite, whitemat, avg, sigma, xTilde, D] = stanford_white(x,epsilon, k)
 
 %http://ufldl.stanford.edu/wiki/index.php/Implementing_PCA/Whitening
 
@@ -15,17 +14,23 @@ sigma = x * x' / size(x, 2);
 
 %xRot = U' * x;          % rotated version of the data. 
 
-k = 0; % select first k values
-
 svect = diag(S);
-sum_values = sum(svect);
-current_sum = 0;
 
-while (current_sum/sum_values < 0.99)
-    k = k+1;
-    current_sum = sum(svect(1:k));
-end
+%if (2 == 1)
+%    k = 0; % select first k values
+%    sum_values = sum(svect);
+%    current_sum = 0;
 
+%    while (current_sum/sum_values < 0.99)
+%        k = k+1;
+%        current_sum = sum(svect(1:k));
+%    end
+%else
+%k=150;
+%end
+
+
+D = diag(1./sqrt(svect(1:k) + epsilon)) * U(:,1:k)';
 xTilde = diag(1./sqrt(svect(1:k) + epsilon)) * U(:,1:k)' * x; % reduced dimension representation of the data, 
                         % where k is the number of eigenvectors to keep =
                         % PCA whitening
