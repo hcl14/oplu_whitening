54860d5bf04a005f62a5b0d28da739f154192c76
hcl14 <hcl14@i.ua>
2017-08-03 16:08:05 +0300

    fixed path

--- matlab_mnist/dl_augm.sh
+++ matlab_mnist/dl_augm.sh
@@ -6,7 +6,7 @@ mkdir centered
 
 
 wget http://www.cs.toronto.edu/~tijmen/affNIST/32x/transformed/training_batches.zip   #AUGMENTED DATA
-wget http://www.cs.toronto.edu/~tijmen/affNIST/32x/originals/validation.mat.zip       #ORIGINALS
+wget http://www.cs.toronto.edu/~tijmen/affNIST/32x/just_centered/validation.mat.zip       #ORIGINALS
 
 unzip training_batches.zip
 unzip validation.mat.zip -d centered
